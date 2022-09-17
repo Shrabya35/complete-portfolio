87939c21e71afac669f49b4c9335b401f23df70c
Shrabya <97744118+Shrabya35@users.noreply.github.com>
2022-09-17 18:07:42 +0545

    host added

--- hello/settings.py
+++ hello/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h9-0@=-a7c7fgn)g18cxuayc($2!dhhvr&3iz0tcpy#y_nat84
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','shrabya.herokuapp.com']
 
 
 # Application definition
