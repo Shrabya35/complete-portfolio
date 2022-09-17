599593c5223700166a09e3c79aa18a9b10be17e1
Shrabya <97744118+Shrabya35@users.noreply.github.com>
2022-09-17 18:18:32 +0545

    Update urls.py

--- hello/urls.py
+++ hello/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
+from django.conf.urls import url
 
 admin.site.site_header = "Medicure Admin"
 admin.site.site_title = "Medicure Admin Portal"
